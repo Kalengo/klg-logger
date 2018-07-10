3b7b41880fb3c8f1cd628bdc3f1fdf405c0fab2d
Nick <myfjdthink@163.com>
2018-07-10 11:33:21 +0800

    update tracer to 0.9.1

--- dist/Logger.d.ts
+++ dist/Logger.d.ts
@@ -1,29 +1,10 @@
 import { Tracer } from 'tracer';
-export declare type KlgTransportFunction = (data: Tracer.LogOutput) => void;
-/**
- * Tracer 的 tsd 文件把 transport 参数类型搞错了，这里更正一下，已经 PR
- * typescript 不支持 overwrite interface property，所以这里只能重新定义一遍了
- */
-export interface LoggerConfig {
-    format?: string;
-    dateformat?: string;
-    filters?: Tracer.FilterFunction[];
-    level?: string;
-    methods?: string[];
-    stackIndex?: number;
-    inspectOpt?: {
-        showHidden: boolean;
-        depth: number;
-    };
-    preprocess?(data: Tracer.LogOutput): void;
-    transport?: KlgTransportFunction | KlgTransportFunction[];
-}
 /**
  * console 是一个没有 constructor 属性的 function，不能直接 class Logger extends console
  * 所以需要通过 Function 来做一个桥接，在 Function 的 constructor 中 return console 实例
  */
 export declare class Logger extends Function implements Tracer.Logger {
-    constructor(config?: LoggerConfig);
+    constructor(config?: Tracer.LoggerConfig);
     debug(...args: any[]): Tracer.LogOutput;
     err(...args: any[]): Tracer.LogOutput;
     error(...args: any[]): Tracer.LogOutput;
--- src/Logger.ts
+++ src/Logger.ts
@@ -1,37 +1,11 @@
 import {console, Tracer} from 'tracer'
 
-export type KlgTransportFunction = (data: Tracer.LogOutput) => void
-
-/**
- * Tracer 的 tsd 文件把 transport 参数类型搞错了，这里更正一下，已经 PR https://github.com/baryon/tracer/pull/87
- * typescript 不支持 overwrite interface property，所以这里只能重新定义一遍了
- */
-export interface LoggerConfig {
-  format?: string
-  dateformat?: string
-  filters?: Tracer.FilterFunction[]
-  level?: string
-  methods?: string[]
-  /* get the specified index of stack as file information. It is userful for development package. */
-  stackIndex?: number
-  inspectOpt?: {
-    /* if true then the object's non-enumerable properties will be shown too. Defaults to false */
-    showHidden: boolean,
-    depth: number
-  }
-
-  preprocess? (data: Tracer.LogOutput): void
-
-  transport?: KlgTransportFunction | KlgTransportFunction[]
-}
-
-
 /**
  * console 是一个没有 constructor 属性的 function，不能直接 class Logger extends console
  * 所以需要通过 Function 来做一个桥接，在 Function 的 constructor 中 return console 实例
  */
 export class Logger extends Function implements Tracer.Logger {
-  constructor (config?: LoggerConfig) {
+  constructor (config?: Tracer.LoggerConfig) {
     super()
     Object.setPrototypeOf(console, Logger.prototype)
     const instance = console(config as any) as Logger
